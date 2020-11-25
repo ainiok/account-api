e5626688a30a5d03c9a03e872375625c9cc186d1
xiaojin <admin@ainiok.com>
2020-11-25 17:00:55 +0800

    [add] 增加swagger

---
+++ app/Http/Controllers/ApiDocsController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Author: xiaojin
+ * Email: job@ainiok.com
+ * Date: 2020/11/25 15:31
+ */
+
+namespace App\Http\Controllers;
+
+/**
+ * @OA\Info(
+ *     title="接口文档",
+ *     version="1.0.1",
+ *     @OA\Contact(email="job@ainiok.com"),
+ *     @OA\License(name="MIT", url="http://github.com/gruntjs/grunt/blob/master/LICENSE-MIT")
+ * )
+ */
+class ApiDocsController extends Controller
+{
+    // api
+}
