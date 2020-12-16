919f1117819396c2974caf45f88804b61a526669
xiaojin <admin@ainiok.com>
2020-12-16 18:15:42 +0800

    [update] 修改admin密码

--- app/Common/Constant.php
+++ app/Common/Constant.php
@@ -11,5 +11,5 @@ class Constant
 {
     const ADMIN_EMAIL = 'admin@ainiok.com';
     const ADMIN_UID = 'd9b877f7fd8211e68674fefcfed11f3c';
-    const ADMIN_INIT_PWD = 'root123';
+    const ADMIN_INIT_PWD = '$2y$10$RplRjXlqCre9jA/nk2EZsuzEDJ0lWFPlw09jTyp1ASL2VkfyFouNy'; // Admin@1212
 }
