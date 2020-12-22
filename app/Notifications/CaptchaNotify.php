46bac4c05a3f299814307a8fd023a86b868c55fb
xiaojin <admin@ainiok.com>
2020-12-22 21:12:21 +0800

    [add] 验证码邮件通知以及邮件模版

--- app/Notifications/CaptchaNotify.php
+++ app/Notifications/CaptchaNotify.php
@@ -7,7 +7,6 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
-use Illuminate\Support\Facades\Log;
 
 class CaptchaNotify extends Notification
 {
@@ -34,7 +33,7 @@ class CaptchaNotify extends Notification
      */
     public function via($notifiable)
     {
-        switch ($notifiable){
+        switch ($this->driver) {
             case 'sms':
                 return [];
             case 'mail':
@@ -52,15 +51,10 @@ class CaptchaNotify extends Notification
      */
     public function toMail($notifiable)
     {
-        Log::info(json_encode($notifiable));
         $code = MailCaptcha::genCodeAndStore($notifiable->email);
         return (new MailMessage())->view(
             'emails.code', ['code' => $code]
         )->subject(trans('email.captcha.subject'));
-//        return (new MailMessage)
-//                    ->line('The introduction to the notification.')
-//                    ->action('Notification Action', url('/'))
-//                    ->line('Thank you for using our application!');
     }
 
     /**
--- resources/lang/zh-CN/passwords.php
+++ resources/lang/zh-CN/passwords.php
@@ -13,14 +13,14 @@ return [
     |
     */
 
-    'password' => 'Passwords must be at least six characters and match the confirmation.',
+    'password' => '密码必须至少为六个字符，并且与确认密码一致.',
     'reset'    => [
         'mail_subject' => '账户密码重置邮件',
         'mail_title'   => '请点击以下链接，完成您账户的密码重置。(该链接仅在24小时内访问有效，24小时后请重新进行该账户的重置操作流程。)',
         'mail_footer'  => '如果以上链接无法访问，请将该网址复制并粘贴至浏览器窗口中直接访问。',
     ],
-    'sent'     => 'We have e-mailed your password reset link!',
-    'token'    => 'This password reset token is invalid.',
-    'user'     => "We can't find a user with that e-mail address.",
+    'sent'     => '我们已通过电子邮件发送了您的密码重置链接!',
+    'token'    => '密码重置令牌无效.',
+    'user'     => "我们找不到使用该电子邮件地址的用户.",
 
 ];
